5a4b9cfffe69b94643c773ee9e1c7ca99f8ae28c
LC-daoxin <457121827@qq.com>
2021-09-14 01:33:58 +0800

    Project reinitialization

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,35 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+
+  extends: 'vuetify',
+
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-console': 'off',
+    'no-debugger': 'off',
+    // https://github.com/babel/babel-eslint/issues/681#issuecomment-420663038
+    'template-curly-spacing': 'off',
+    indent: 'off',
+    //
+    'no-unused-vars': 'warn',
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
+import vuetify from './plugins/vuetify'
+import './plugins'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
-  render: h => h(App)
+  vuetify,
+  render: h => h(App),
 }).$mount('#app')
